328d032f8282d4364e580312697b4cd590bb4a0d
Ksawery Lejczak <79964602+Cloudy17g35@users.noreply.github.com>
2021-12-25 08:53:45 +0100

    Create TwoOldestAges.py

---
+++ TwoOldestAges.py
@@ -0,0 +1,6 @@
+'''The two oldest ages function/method needs to be completed. 
+It should take an array of numbers as its argument and return the two highest numbers within the array.'''
+
+
+def two_oldest_ages(ages):
+    return sorted(ages)[-2:]  
