b8805fd3f77a6052e59c2fec49fd61f935382177
Ksawery Lejczak <79964602+Cloudy17g35@users.noreply.github.com>
2021-12-28 21:01:49 +0100

    Create Primorial Of a Number.py

---
+++ Primorial Of a Number.py
@@ -0,0 +1,34 @@
+import math
+def is_prime(n):
+    """
+    Assumes that n is a positive natural number
+    """
+    # We know 1 is not a prime number
+    if n <= 1:
+        return False
+
+    i = 2
+    # This will loop from 2 to int(sqrt(x))
+    while i*i <= n:
+        # Check if i divides x without leaving a remainder
+        if n % i == 0:
+            # This means that n has a factor in between 2 and sqrt(n)
+            # So it is not a prime number
+            return False
+        i += 1
+    # If we did not find any factor in the above loop,
+    # then n is a prime number
+    return True
+
+def num_primorial(n):
+    primes = [2]
+
+    i = 3
+    while len(primes) != n:
+        if is_prime(i):
+            primes.append(i)
+        i+= 2
+
+
+
+    return math.prod(primes)
